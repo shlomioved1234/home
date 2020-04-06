8a85d81375b33675b6030b854503fe4aeee84abb
shlomioved1234 <shlomioved123@gmail.com>
2020-04-05 21:17:08 -0400

    changing footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,21 +9,21 @@ const Footer = () => {
   return (
     <footer style={bgStyle} className="mt-auto py-3 text-center">
       {/* <strong> &copy; 2019 </strong>*/}
-      <i className="fas fa-code"></i> with <i className="fas fa-heart"></i> by{" "}
+      <i className="fas fa-code"></i> by{" "}
       <a
         className="badge badge-dark"
         rel="noopener"
-        href="https://github.com/hashirshoaeb"
+        href="https://github.com/shlomioved1234"
         aria-label="My GitHub"
       >
-        Hashir Shoaib
+        Shlomi Oved
       </a>{" "}
       using <i className="fab fa-react"></i>
       <p>
         <small className="text-muted">
           {" "}
-          Project code is open source. Feel free to fork and make your own
-          version.
+
+
         </small>
       </p>
     </footer>

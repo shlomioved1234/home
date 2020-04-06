87ef031af139de5646cd38bb2adc50da7a06ab0d
shlomioved1234 <shlomioved123@gmail.com>
2020-04-05 20:47:47 -0400

    Updates

---
+++ precache-manifest.291153e1092ffad9546ed557cad72ca1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91c748018c89c5e6414007fbf1782ba",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "938fc60791f4935701a2",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "53361d472deb72e85a86",
+    "url": "/home/static/css/main.1f7ed24e.chunk.css"
+  },
+  {
+    "revision": "938fc60791f4935701a2",
+    "url": "/home/static/js/2.e9656ac1.chunk.js"
+  },
+  {
+    "revision": "76fd5ab146b0d8fb7b899b4b882f747e",
+    "url": "/home/static/js/2.e9656ac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53361d472deb72e85a86",
+    "url": "/home/static/js/main.bd8cb696.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "adf5af305637c87360fb2caf2384a704",
+    "url": "/home/static/media/resume.adf5af30.pdf"
+  }
+]);
